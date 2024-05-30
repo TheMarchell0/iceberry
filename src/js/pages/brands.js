6f3a4e95b79eb59e24fe9bf7a22cb21f90547001
unknown <TheMarchell0@yandex.ru>
2024-05-31 01:07:44 +0600

    create slider functional on brands page

--- src/js/pages/brands.js
+++ src/js/pages/brands.js
@@ -1,20 +1,31 @@
 document.addEventListener('DOMContentLoaded', function () {
-        const heroSlider = new Swiper(".js-hero-slider", {
-            loop: true,
-            pagination: {
-                el: '.hero__pagination',
-                type: 'bullets',
-                clickable: true,
-            },
-            effect: 'fade',
-            speed: 800,
-            autoplay: {
-                delay: 5000,
-            },
-            slidesPerView: 1,
-        });
+    const parent = document.querySelector('.js-slider-parent'),
+        pagination = parent.querySelector('.js-hero-pagination'),
+        header = document.querySelector('.js-header');
+
+    let activeTheme = 'vologodskiy-plombir';//При загрузке страницы слайдер начинается с Вологодского пломбира
+    header.classList.add(activeTheme);
+
+    const heroSlider = new Swiper(".js-hero-slider", {
+        loop: true,
+        pagination: {
+            el: '.hero__pagination',
+            type: 'bullets',
+            clickable: true,
+        },
+        effect: 'fade',
+        slidesPerView: 1,
+        allowTouchMove: false,
+    });
 
     heroSlider.on('slideChange', function () {
-        console.log(1);
+        setTimeout(() => {
+            let newTheme = parent.querySelector('.js-brands-slide.swiper-slide-active').getAttribute('data-theme');
+            pagination.classList.remove(activeTheme);
+            header.classList.remove(activeTheme);
+            pagination.classList.add(newTheme);
+            header.classList.add(newTheme)
+            activeTheme = newTheme;
+        }, 100)
     });
 });
