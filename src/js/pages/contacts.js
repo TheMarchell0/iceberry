426504e4de42803577689f9fce029a68ddede71d
unknown <TheMarchell0@yandex.ru>
2024-07-26 17:53:34 +0600

    finish

--- src/js/pages/contacts.js
+++ src/js/pages/contacts.js
@@ -9,16 +9,18 @@ import {openModalFunctional} from "../helpers/openModalFunctional";
 import {createTabsFunctional} from "../helpers/createTabsFunctional";
 
 document.addEventListener('DOMContentLoaded', function () {
-    const parent = document.querySelector('.js-info-parent'),
-        infoSliderBlock = parent.querySelector('.js-info-slider'),
+    const infoContentBlocks = document.querySelectorAll('.js-option-content'),
         elements = document.querySelectorAll('.js-choices-select'),
-        forms = document.querySelectorAll('.js-form');
+        contactsSelect = document.querySelector('.js-contacts-select'),
+        forms = document.querySelectorAll('.js-form'),
+        screenWidth = window.innerWidth;
 
     fileInputInitialization(forms);
     phoneMaskInitialization();
     openModalFunctional();
     createFormValidation(forms);
     createTabsFunctional();
+
 
     for (let element of elements) {
         const specificType = element.getAttribute('data-select-type');
@@ -34,24 +36,53 @@ document.addEventListener('DOMContentLoaded', function () {
         });
     }
 
-    const infoSlider = new Swiper(infoSliderBlock, {
-        loop: false,
-        navigation: {
-            nextEl: ".info__slider-next",
-            prevEl: ".info__slider-prev",
-        },
-        spaceBetween: 20,
-        breakpoints: {
-            320: {
-                slidesPerView: '1.11',
-                spaceBetween: 10,
-            },
-            600: {
-                slidesPerView: '2',
-            },
-            1024: {
-                slidesPerView: '3',
-            },
-        },
+    contactsSelect.addEventListener('change', function() {
+        const value = contactsSelect.value;
+        for (let infoContentBlock of infoContentBlocks) {
+            if (infoContentBlock.classList.contains('info_active') && infoContentBlock.getAttribute('data-value-content') !== value) {
+                infoContentBlock.classList.remove('info_active')
+            }
+
+            if (infoContentBlock.getAttribute('data-value-content') === value) {
+                infoContentBlock.classList.add('info_active')
+            }
+        }
     });
+
+
+    for (let infoContentBlock of infoContentBlocks) {
+        const items = infoContentBlock.querySelectorAll('.swiper-slide').length;
+
+        if ((items > 3) || (items  === 3 && screenWidth < 1024) || (items === 2 && screenWidth < 660)) {
+
+            if (!infoContentBlock.classList.contains('info__options-item_slider')) {
+                infoContentBlock.classList.add('info__options-item_slider');
+            }
+
+            const infoSlider = new Swiper(infoContentBlock.querySelector('.js-info-slider'), {
+                loop: false,
+                navigation: {
+                    nextEl: infoContentBlock.querySelector('.info__slider-next'),
+                    prevEl: infoContentBlock.querySelector('.info__slider-prev'),
+                },
+                spaceBetween: 21,
+                breakpoints: {
+                    320: {
+                        slidesPerView: '1.11',
+                        spaceBetween: 10,
+                    },
+                    660: {
+                        slidesPerView: '2',
+                    },
+                    1024: {
+                        slidesPerView: '3',
+                    },
+                },
+            });
+        }
+    }
 })
+
+function openInfoContent() {
+
+}
